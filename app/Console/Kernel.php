5b0266d3f9600addbc208fc89bde06362e3e02cf
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2023-08-20 18:39:09 +0700

    add queue and schedule on docker compose development

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
 
         $schedule->call(function () {
-            echo 'berhasil run schedul';
+            echo 'berhasil run schedule';
         })->everyMinute();
     }
 
